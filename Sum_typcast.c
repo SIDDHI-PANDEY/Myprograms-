310aeababe2ee1c6c0d203663184f81a868171a1
SIDDHI-PANDEY <arapanap5@gmail.com>
2024-11-01 10:35:52 +0530

    Add files via upload

---
+++ Sum_typcast.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//#include<conio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    float sum;
+    printf("enter any number a ");
+    scanf("%d",&a);
+    printf("enter any number b ");
+    scanf("%d",&b);
+    printf("enter any number c ");
+    scanf("%d",&c);
+    printf("enter any number d ");
+    scanf("%d",&d);
+   
+    e=a/b;
+   f=c/d;
+    sum=e+f;
+    printf(" The value of a/b  is : %d \n",e);
+    printf(" The value of c/d is : %d \n",f);
+    printf(" The sum of div of a/b and c/d is : %f\n",sum);
+return 0;
+} 
