9cb702f0aed0e4e76d4ab1de06bb1928d9db0531
SIDDHI-PANDEY <arapanap5@gmail.com>
2024-11-01 10:16:39 +0530

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+//#include<conio.h>
+int main()
+{
+    int a,b,c,d,e,f,w;
+    float sum;
+    printf("enter any number a ");
+    scanf("%d",&a);
+    printf("enter any number b ");
+    scanf("%d",&b);
+    printf("enter any number c ");
+    scanf("%d",&c);
+    printf("enter any number d ");
+    scanf("%d",&d);
+   
+    e=a/b;
+   f=c/d;
+    sum=e+f;
+    printf(" The value of a/b  is : %d \n",e);
+    printf(" The value of c/d is : %d \n",f);
+    printf(" The sum of div of a/b and c/d is : %f\n",sum);
+    printf("Enter your name:");
+scanf("%d",&w);
+return 0;
+} 
